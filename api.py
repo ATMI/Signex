36c1673aa13487032e20ab44d8a9a5dbcda43792
a <artem.myasoedov@gmail.com>
2023-07-09 21:19:08 +0300

    Adding multithread API support

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 import cgi
-from http import HTTPStatus
+from enum import Enum
+from http.client import INTERNAL_SERVER_ERROR, BAD_REQUEST, OK, NOT_IMPLEMENTED
+from typing import Dict, Tuple, Callable
 
 import cv2
 import numpy as np
@@ -8,62 +10,156 @@ from flup.server.fcgi import WSGIServer
 from detector import Detector
 
 
-def detect(environ):
-	content_type = environ.get("CONTENT_TYPE")
-	if not content_type or not content_type.startswith("multipart/form-data"):
-		return HTTPStatus.BAD_REQUEST, "Invalid input format\n", [("Content-Type", "text/plain")]
+class Header(Enum):
+	class Key(Enum):
+		CONTENT_TYPE = "Content-Type"
+		CONTENT_DISPOSITION = "Content-Dispositon"
 
-	input_stream = environ.get("wsgi.input")
-	form = cgi.FieldStorage(fp=input_stream, environ=environ)
+	class Value(Enum):
+		PLAIN_TEXT = "text/plain"
+		MULTIPART_FD = "multipart/form-data"
+		IMAGE_JPEG = "image/jpeg"
 
-	image_field = form.getvalue("image")
-	if image_field is None:
-		return HTTPStatus.BAD_REQUEST, "No valid 'image' to process was provided\n", [("Content-Type", "text/plain")]
-
-	image_data = image_field.file.read()
-	image_array = np.frombuffer(image_data, dtype=np.uint8)
-	image_mat = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-
-	result = detector.detect(image_mat)
-	(ok, result_data) = cv2.imencode(".jpg", result)
-
-	if not ok:
-		return HTTPStatus.INTERNAL_SERVER_ERROR, "Failed to process image\n", [("Content-Type", "text/plain")]
-
-	headers = [("Content-Type", "image/jpeg"), ("Content-Dispositon", "inline; filename=result.jpg")]
-	return HTTPStatus.OK, result_data.tobytes(), headers
+	CONTENT_PLAIN_TEXT = (Key.CONTENT_TYPE, Value.PLAIN_TEXT)
+	CONTENT_MULTIPART_FD = (Key.CONTENT_TYPE, Value.MULTIPART_FD)
+	CONTENT_JPEG = (Key.CONTENT_TYPE, Value.IMAGE_JPEG)
 
 
-handlers = {
-	("/detect", "POST"): detect
-}
+class CgiField(Enum):
+	DOCUMENT_URI = "DOCUMENT_URI"
+	REQUEST_METHOD = "REQUEST_METHOD"
+	CONTENT_TYPE = "CONTENT_TYPE"
 
 
-def api(environ, start_response):
-	status: HTTPStatus = HTTPStatus.INTERNAL_SERVER_ERROR
-	response = "Some inner error occurred, please contact the developers to report the issue\n"
-	headers = [("Content-Type", "text/plain")]
+class InternalError(Enum):
+	UNKNOWN = -1
+	NO_CGI_INPUT = -2
+	CANT_READ_CGI_FIELD = -3
+	CANT_ENCODE_IMAGE = -4
 
-	try:
-		document_uri = environ["DOCUMENT_URI"]
-		request_method = environ["REQUEST_METHOD"]
-		handler = handlers.get((document_uri, request_method))
 
-		if handler:
-			status, response, headers = handler(environ)
-		else:
-			status = HTTPStatus.METHOD_NOT_ALLOWED
-			response = "Unknown method\n"
+class Response:
+	def __init__(self, status: int, body, headers: [Header]):
+		self.status = status
+		self.body = body
+		self.headers = headers
 
-	except Exception as e:
-		# TODO: handle error appropriately
-		print(e)
-		pass
-	finally:
-		start_response(f"{status.value} {status.phrase}", headers)
-		return [response]
+	@classmethod
+	def error(cls, code: int, message: str) -> 'Response':
+		return cls(code, message, [Header.CONTENT_PLAIN_TEXT])
+
+	@classmethod
+	def bad_request(cls, message: str) -> 'Response':
+		return cls.error(BAD_REQUEST, message)
+
+	@classmethod
+	def ok(cls, body, headers):
+		return cls(OK, body, headers)
+
+	@classmethod
+	def internal_server_error(cls, code: InternalError) -> 'Response':
+		return cls.error(INTERNAL_SERVER_ERROR, f"Internal server error occurred, code: {code.value}\n")
+
+	@classmethod
+	def no_required_field(cls, field_name) -> 'Response':
+		return cls.bad_request(f"Can not find required field: '{field_name}'\n")
+
+	@classmethod
+	def no_such_method(cls, method, name) -> 'Response':
+		return cls.bad_request(f"Can not find requested method: '{method}: {name}'\n")
+
+	@classmethod
+	def invalid_content_type(cls, provided, expected) -> 'Response':
+		return cls.bad_request(f"Invalid content type:\nProvided: '{provided}'\nExpected: '{expected}\n")
+
+	@classmethod
+	def not_implemented(cls) -> 'Response':
+		return cls.error(NOT_IMPLEMENTED, "This method is not yet implemented\n")
+
+
+class Api:
+	def __init__(self, address, detector, log):
+		self.log = log
+		self.detector = detector
+		self.server = WSGIServer(self.accept_request, bindAddress=address)
+		self.handlers: Dict[Tuple[str, str], Callable[[dict], Response]] = {
+			("/detect", "POST"): self.detect,
+			("/compare", "POST"): self.compare
+		}
+
+	def handle_request(self, environ: dict) -> Response:
+		request_method = environ.get(CgiField.REQUEST_METHOD)
+		if request_method is None:
+			return Response.no_required_field(CgiField.REQUEST_METHOD)
+
+		document_uri = environ.get(CgiField.DOCUMENT_URI)
+		if document_uri is None:
+			return Response.no_required_field(CgiField.DOCUMENT_URI)
+
+		handler = self.handlers.get((document_uri, request_method))
+		if handler is None:
+			return Response.no_such_method(request_method, document_uri)
+
+		return handler(environ)
+
+	def accept_request(self, environ: dict, start_response):
+		response = Response.internal_server_error(InternalError.UNKNOWN)
+		try:
+			response = self.handle_request(environ)
+		except Exception as e:
+			self.log(e)
+		finally:
+			start_response(f"{response.status}", response.headers)
+			return [response.body]
+
+	async def detect(self, environ) -> Response:
+		content_type = environ.get(CgiField.CONTENT_TYPE)
+		if not content_type or not content_type.startswith(Header.Value.MULTIPART_FD):
+			return Response.invalid_content_type(content_type, Header.Value.MULTIPART_FD)
+
+		input_stream = environ.get("wsgi.input")
+		if not input_stream:
+			return Response.internal_server_error(InternalError.NO_CGI_INPUT)
+
+		form = cgi.FieldStorage(fp=input_stream, environ=environ)
+
+		image_field = form.getvalue("image")
+		if image_field is None:
+			return Response.no_required_field("image")
+
+		try:
+			image_data = image_field.file.read()
+		except Exception as e:
+			self.log(e)
+			return Response.internal_server_error(InternalError.CANT_READ_CGI_FIELD)
+
+		try:
+			image_array = np.frombuffer(image_data, dtype=np.uint8)
+			image_mat = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+		except Exception as e:
+			self.log(e)
+			return Response.bad_request("Can not decode provided image\n")
+
+		result = await self.detector.detect(image_mat)
+		(ok, result_data) = cv2.imencode(".jpg", result)
+
+		if not ok:
+			return Response.internal_server_error(InternalError.CANT_ENCODE_IMAGE)
+
+		headers = [Header.CONTENT_JPEG, (Header.Key.CONTENT_DISPOSITION, "inline; filename=result.jpg")]
+		return Response.ok(result_data.tobytes(), headers)
+
+	def compare(self, environ) -> Response:
+		return Response.not_implemented()
+
+	def run(self):
+		self.server.run()
 
 
 if __name__ == "__main__":
-	detector = Detector("weights/best.pt")
-	WSGIServer(api, bindAddress=("localhost", 8080)).run()
+	try:
+		detector = Detector("weights/best.pt", max_workers=1)
+		api = Api(("localhost", 8080), detector, print)
+		api.run()
+	except Exception as e:
+		print(e)
--- detector.py
+++ detector.py
@@ -1,4 +1,8 @@
+import queue
 import sys
+import threading
+from concurrent.futures import ThreadPoolExecutor, Future
+from enum import Enum
 
 import numpy as np
 import torch
@@ -14,42 +18,70 @@ from yolov7.utils.general import check_img_size, non_max_suppression, scale_coor
 from yolov7.utils.plots import plot_one_box
 
 
+class Task(Enum):
+	STOP = 0,
+	DETECT = 1
+
+
 class Detector:
-	def __init__(self, weights, img_size=640):
+
+	def __init__(self, weights, max_workers, img_size=640):
+		self.queue = queue.Queue()
+		self.weights = weights
+		self.img_size = img_size
+		self.executor = ThreadPoolExecutor(max_workers=max_workers)
+		for _ in range(max_workers):
+			self.executor.submit(self.worker)
+
+	def worker(self):
 		with torch.no_grad():
-			self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-			self.model = attempt_load([weights], self.device)
+			device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+			model = attempt_load([self.weights], device)
+			stride = int(model.stride.max())
+			img_size = check_img_size(self.img_size, s=stride)
+			names = model.module.names if hasattr(model, "module") else model.names
+			colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
 
-			self.stride = int(self.model.stride.max())
-			self.img_size = check_img_size(img_size, s=self.stride)
+			def __detect__(image, conf_thresh=0.45, iou_thresh=0.45):
+				img = yolov7.utils.datasets.letterbox(image, img_size, stride=stride)[0]
 
-			self.names = self.model.module.names if hasattr(self.model, "module") else self.model.names
-			self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in self.names]
+				# Convert
+				img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+				img = np.ascontiguousarray(img)
+				img = torch.from_numpy(img).to(device)
+				img = img.float()
+				img /= 255.0  # 0 - 255 to 0.0 - 1.0
+				if img.ndimension() == 3:
+					img = img.unsqueeze(0)
 
-	def detect(self, image, conf_thresh=0.45, iou_thresh=0.45):
-		img = yolov7.utils.datasets.letterbox(image, self.img_size, stride=self.stride)[0]
+				with torch.no_grad():
+					pred = model(img)[0]
+				pred = non_max_suppression(pred, conf_thresh, iou_thresh, classes=None, agnostic=False)
 
-		# Convert
-		img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
-		img = np.ascontiguousarray(img)
-		img = torch.from_numpy(img).to(self.device)
-		img = img.float()
-		img /= 255.0  # 0 - 255 to 0.0 - 1.0
-		if img.ndimension() == 3:
-			img = img.unsqueeze(0)
+				for i, det in enumerate(pred):  # detections per image
+					if len(det):
+						# Rescale boxes from img_size to im0 size
+						gn = torch.tensor(image.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+						det[:, :4] = scale_coords(img.shape[2:], det[:, :4], image.shape).round()
 
-		with torch.no_grad():
-			pred = self.model(img)[0]
-		pred = non_max_suppression(pred, conf_thresh, iou_thresh, classes=None, agnostic=False)
+						for *xyxy, conf, cls in reversed(det):
+							label = f'{names[int(cls)]} {conf:.2f}'
+							plot_one_box(xyxy, image, label=label, color=colors[int(cls)], line_thickness=1)
 
-		for i, det in enumerate(pred):  # detections per image
-			if len(det):
-				# Rescale boxes from img_size to im0 size
-				gn = torch.tensor(image.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-				det[:, :4] = scale_coords(img.shape[2:], det[:, :4], image.shape).round()
+				return image
 
-				for *xyxy, conf, cls in reversed(det):
-					label = f'{self.names[int(cls)]} {conf:.2f}'
-					plot_one_box(xyxy, image, label=label, color=self.colors[int(cls)], line_thickness=1)
+			while True:
+				(task, args) = self.queue.get()
+				match task:
+					case Task.STOP:
+						break
+					case Task.DETECT:
+						(image, future) = args
+						result = __detect__(image)
+						future.set_result(result)
+						self.queue.task_done()
 
-		return image
+	def detect(self, image) -> Future:
+		future = Future()
+		self.queue.put((Task.DETECT, (image, future)))
+		return future
