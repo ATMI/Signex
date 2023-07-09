f65fc44e672817879309cad9bbbfa1d8903d5093
a <artem.myasoedov@gmail.com>
2023-07-09 22:19:20 +0300

    Reimplemented API in a better manner

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import cgi
-from enum import Enum
+from http import HTTPStatus
 from http.client import INTERNAL_SERVER_ERROR, BAD_REQUEST, OK, NOT_IMPLEMENTED
 from typing import Dict, Tuple, Callable
 
@@ -9,44 +9,37 @@ from flup.server.fcgi import WSGIServer
 
 from detector import Detector
 
+CONTENT_TYPE = "Content-Type"
+CONTENT_DISPOSITION = "Content-Dispositon"
 
-class Header(Enum):
-	class Key(Enum):
-		CONTENT_TYPE = "Content-Type"
-		CONTENT_DISPOSITION = "Content-Dispositon"
+PLAIN_TEXT = "text/plain"
+MULTIPART_FD = "multipart/form-data"
+IMAGE_JPEG = "image/jpeg"
 
-	class Value(Enum):
-		PLAIN_TEXT = "text/plain"
-		MULTIPART_FD = "multipart/form-data"
-		IMAGE_JPEG = "image/jpeg"
+CONTENT_PLAIN_TEXT = (CONTENT_TYPE, PLAIN_TEXT)
+CONTENT_MULTIPART_FD = (CONTENT_TYPE, MULTIPART_FD)
+CONTENT_JPEG = (CONTENT_TYPE, IMAGE_JPEG)
 
-	CONTENT_PLAIN_TEXT = (Key.CONTENT_TYPE, Value.PLAIN_TEXT)
-	CONTENT_MULTIPART_FD = (Key.CONTENT_TYPE, Value.MULTIPART_FD)
-	CONTENT_JPEG = (Key.CONTENT_TYPE, Value.IMAGE_JPEG)
+CGI_DOCUMENT_URI = "DOCUMENT_URI"
+CGI_REQUEST_METHOD = "REQUEST_METHOD"
+CGI_CONTENT_TYPE = "CONTENT_TYPE"
+CGI_INPUT = "wsgi.input"
 
-
-class CgiField(Enum):
-	DOCUMENT_URI = "DOCUMENT_URI"
-	REQUEST_METHOD = "REQUEST_METHOD"
-	CONTENT_TYPE = "CONTENT_TYPE"
-
-
-class InternalError(Enum):
-	UNKNOWN = -1
-	NO_CGI_INPUT = -2
-	CANT_READ_CGI_FIELD = -3
-	CANT_ENCODE_IMAGE = -4
+UNKNOWN = -1
+NO_CGI_INPUT = -2
+CANT_READ_CGI_FIELD = -3
+CANT_ENCODE_IMAGE = -4
 
 
 class Response:
-	def __init__(self, status: int, body, headers: [Header]):
+	def __init__(self, status: int, body, headers: [(str, str)]):
 		self.status = status
 		self.body = body
 		self.headers = headers
 
 	@classmethod
 	def error(cls, code: int, message: str) -> 'Response':
-		return cls(code, message, [Header.CONTENT_PLAIN_TEXT])
+		return cls(code, message, [CONTENT_PLAIN_TEXT])
 
 	@classmethod
 	def bad_request(cls, message: str) -> 'Response':
@@ -57,8 +50,8 @@ class Response:
 		return cls(OK, body, headers)
 
 	@classmethod
-	def internal_server_error(cls, code: InternalError) -> 'Response':
-		return cls.error(INTERNAL_SERVER_ERROR, f"Internal server error occurred, code: {code.value}\n")
+	def internal_server_error(cls, code: int) -> 'Response':
+		return cls.error(INTERNAL_SERVER_ERROR, f"Internal server error occurred, code: {code}\n")
 
 	@classmethod
 	def no_required_field(cls, field_name) -> 'Response':
@@ -88,13 +81,13 @@ class Api:
 		}
 
 	def handle_request(self, environ: dict) -> Response:
-		request_method = environ.get(CgiField.REQUEST_METHOD)
+		request_method = environ.get(CGI_REQUEST_METHOD)
 		if request_method is None:
-			return Response.no_required_field(CgiField.REQUEST_METHOD)
+			return Response.no_required_field(CGI_REQUEST_METHOD)
 
-		document_uri = environ.get(CgiField.DOCUMENT_URI)
+		document_uri = environ.get(CGI_DOCUMENT_URI)
 		if document_uri is None:
-			return Response.no_required_field(CgiField.DOCUMENT_URI)
+			return Response.no_required_field(CGI_DOCUMENT_URI)
 
 		handler = self.handlers.get((document_uri, request_method))
 		if handler is None:
@@ -103,35 +96,30 @@ class Api:
 		return handler(environ)
 
 	def accept_request(self, environ: dict, start_response):
-		response = Response.internal_server_error(InternalError.UNKNOWN)
+		response = Response.internal_server_error(UNKNOWN)
 		try:
 			response = self.handle_request(environ)
 		except Exception as e:
 			self.log(e)
 		finally:
-			start_response(f"{response.status}", response.headers)
+			status = HTTPStatus(response.status)
+			start_response(f"{status.value} {status.phrase}", response.headers)
 			return [response.body]
 
-	async def detect(self, environ) -> Response:
-		content_type = environ.get(CgiField.CONTENT_TYPE)
-		if not content_type or not content_type.startswith(Header.Value.MULTIPART_FD):
-			return Response.invalid_content_type(content_type, Header.Value.MULTIPART_FD)
+	def detect(self, environ) -> Response:
+		content_type = environ.get(CGI_CONTENT_TYPE)
+		if not content_type or not content_type.startswith(MULTIPART_FD):
+			return Response.invalid_content_type(content_type, MULTIPART_FD)
 
-		input_stream = environ.get("wsgi.input")
+		input_stream = environ.get(CGI_INPUT)
 		if not input_stream:
-			return Response.internal_server_error(InternalError.NO_CGI_INPUT)
+			return Response.internal_server_error(NO_CGI_INPUT)
 
 		form = cgi.FieldStorage(fp=input_stream, environ=environ)
 
-		image_field = form.getvalue("image")
-		if image_field is None:
+		image_data = form.getvalue("image")
+		if image_data is None:
 			return Response.no_required_field("image")
-
-		try:
-			image_data = image_field.file.read()
-		except Exception as e:
-			self.log(e)
-			return Response.internal_server_error(InternalError.CANT_READ_CGI_FIELD)
 
 		try:
 			image_array = np.frombuffer(image_data, dtype=np.uint8)
@@ -140,13 +128,13 @@ class Api:
 			self.log(e)
 			return Response.bad_request("Can not decode provided image\n")
 
-		result = await self.detector.detect(image_mat)
+		result = self.detector.detect(image_mat).result()
 		(ok, result_data) = cv2.imencode(".jpg", result)
 
 		if not ok:
-			return Response.internal_server_error(InternalError.CANT_ENCODE_IMAGE)
+			return Response.internal_server_error(CANT_ENCODE_IMAGE)
 
-		headers = [Header.CONTENT_JPEG, (Header.Key.CONTENT_DISPOSITION, "inline; filename=result.jpg")]
+		headers = [CONTENT_JPEG, (CONTENT_DISPOSITION, "inline; filename=result.jpg")]
 		return Response.ok(result_data.tobytes(), headers)
 
 	def compare(self, environ) -> Response:
@@ -157,9 +145,6 @@ class Api:
 
 
 if __name__ == "__main__":
-	try:
-		detector = Detector("weights/best.pt", max_workers=1)
-		api = Api(("localhost", 8080), detector, print)
-		api.run()
-	except Exception as e:
-		print(e)
+	detector = Detector("weights/best.pt", max_workers=1)
+	api = Api(("localhost", 8080), detector, print)
+	api.run()
